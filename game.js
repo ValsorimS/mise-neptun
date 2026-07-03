22a9e8318c98bc4c5858a206ef009640e2a2bb3b
Mirek <miroslavspetla@icloud.com>
2026-07-03 23:51:21 +0200

    Vstavej

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-// --- SCÉNA ---
+// --- SCÉNA ----
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x1a1a2e);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
